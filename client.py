f40715ab5cfdcab1be99408644621eeeee89ad10
José Augusto <79456742+josaugusto@users.noreply.github.com>
2023-09-20 15:57:21 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,42 @@
+import socket
+import os
+from helpers import get_valid_ip, get_valid_port
+from time import sleep
 
+
+def main():
+    HOST = get_valid_ip()   # inserção do IP (localhost = 127.0.0.1)
+    PORT = get_valid_port() # inserção da Porta (server rodando porta 50000)
+    client((HOST, PORT))
+
+
+def client(ADDR):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_client:
+        socket_client.connect(ADDR)
+        print(f'Conectado ao servidor em {ADDR[0]} {ADDR[1]}')
+
+def menu():
+    if os.name == 'nt':
+        clear_cmd = 'cls'
+    else:
+        clear_cmd = 'clear'
+
+    os.system(clear_cmd)
+    print('\tTela inicial do programa')
+    print('-' * 40)
+    print('Selecione uma das opções abaixo:\n')
+    print('\t0 - Logar no servidor\n\t1 - Sair do programa\n')
+    print('-' * 40)
+
+    while True:
+        op = input('-> ').strip()
+        if op == '0':
+            break
+        elif op == '1':
+            return
+        else:
+            print('Opção invalida!')
+
+    os.system(clear_cmd)
+    main()
+menu()      
